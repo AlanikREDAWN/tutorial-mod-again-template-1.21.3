320117dba1e995bfe8a9e7e8bfff3ab5270cf8be
Alanik <lilia.dostie@socprep.org>
2024-10-29 13:40:00 -0400

    Initial commit

---
+++ src/main/java/net/lilia/tutorialmod/TutorialModAgainDataGenerator.java
@@ -0,0 +1,11 @@
+package net.lilia.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TutorialModAgainDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
